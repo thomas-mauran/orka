6dceef82a128970bbdaccb3d448f7436bb6f4f51
Mauran <thomas.mauran@etu.umontpellier.fr>
2023-08-22 20:08:04 +0200

    fix(lint)
    Signed-off-by: Mauran <thomas.mauran@etu.umontpellier.fr>

--- controller/src/types/Instance_Request.rs
+++
@@ -1,8 +0,0 @@
-use serde::Deserialize;
-use validator::Validate;
-
-#[derive(Debug, Validate, Deserialize)]
-pub struct Instance_Request {
-    #[validate(length(min = 1))]
-    pub workload_id: String,
-}
